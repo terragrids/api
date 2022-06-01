9afc5ebe6ed21f6d29f2f462985ebdb463545a5d
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-06-01 23:15:51 +0100

    Add app test

---
+++ src/app.test.js
@@ -0,0 +1,12 @@
+import { app } from './app.js'
+import request from 'supertest'
+
+describe('app', function () {
+    describe('get root endpoint', function () {
+        it('should return 200 when calling root path', async () => {
+            const response = await request(app.callback()).get('/')
+            expect(response.status).toBe(200)
+            expect(response.text).toBe('terragrids store')
+        })
+    })
+})
