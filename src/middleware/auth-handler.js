3bd128086ece6f318ef962aec95b5a31fb1b7c5e
cylee <chunyitlee@gmail.com>
2022-10-13 16:03:22 +0800

    Add try catch to verification logic

--- src/middleware/auth-handler.js
+++ src/middleware/auth-handler.js
@@ -5,35 +5,44 @@ import { day1, minutes30 } from '../utils/constants.js'
 import { TokenExpiredError } from '../error/token-expired-error.js'
 
 export default async function authHandler(ctx, next) {
-    //check if path is allow for non auth
+    let toCheck
+    let signature
+    let decodedNote
+    let from
+    let to
     const { wallet } = ctx.query
     const token = ctx.headers?.authorization?.split(' ')
     if (token?.length !== 2) throw new TokenInvalidError()
+    try {
+        //converting the base64 encoded tx back to binary data
+        const decodeToken = new Uint8Array(Buffer.from(token[1], 'base64'))
+        //getting a SignedTransaction object from the array buffer
+        const decodedTx = algosdk.decodeSignedTransaction(decodeToken)
 
-    //converting the base64 encoded tx back to binary data
-    const decodeToken = new Uint8Array(Buffer.from(token[1], 'base64'))
-    //getting a SignedTransaction object from the array buffer
-    const decodedTx = algosdk.decodeSignedTransaction(decodeToken)
+        //auth tx whose params we'll check
+        toCheck = decodedTx.txn
 
-    //auth tx whose params we'll check
-    const toCheck = decodedTx.txn
+        // get the signature from the signed transaction
+        signature = decodedTx.sig
 
-    // get the signature from the signed transaction
-    const signature = decodedTx.sig
+        // parse the note back to utf-8
+        const note = new TextDecoder().decode(toCheck.note)
+        decodedNote = note.split(' ')
+        // "from" and "to" are distincts ArrayBuffers,
+        // comparing them directly would always return false.
+        // We therefore convert them back to base32 for comparison.
+        from = algosdk.encodeAddress(toCheck.from.publicKey)
+        to = algosdk.encodeAddress(toCheck.to.publicKey)
+        // Guard clause to make sure the token has not expired.
+        // We also check the token expiration is not too far out, which would be a red flag.
+    } catch (e) {
+        throw new TokenInvalidError()
+    }
 
-    // parse the note back to utf-8
-    const note = new TextDecoder().decode(toCheck.note)
-    const decodedNote = note.split(' ')
-    // "from" and "to" are distincts ArrayBuffers,
-    // comparing them directly would always return false.
-    // We therefore convert them back to base32 for comparison.
-    const from = algosdk.encodeAddress(toCheck.from.publicKey)
-    const to = algosdk.encodeAddress(toCheck.to.publicKey)
-    // Guard clause to make sure the token has not expired.
-    // We also check the token expiration is not too far out, which would be a red flag.
     if (Number(decodedNote[1]) < Date.now() || Number(decodedNote[1]) > Date.now() + day1 + minutes30) {
         throw new TokenExpiredError()
     }
+
     // We check if the params match the ones we set in the front-end
     const env = process.env.ENV === 'dev' ? 'testnet' : 'app'
     if (
